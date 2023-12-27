5e9cae12af4560360c8e0ad842baeca59364da2e
shweta <shwetasinghh9416@gmail.com>
2023-12-27 19:37:43 +0530

    firt commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactRecorder from './components/ReactRecorder';
+
+const App = () => {
+  return (
+    <div className=' bg-slate-800 h-[100vh] pt-10 text-white ' >
+      <ReactRecorder />
+    </div>
+  );
+};
+
+export default App;
